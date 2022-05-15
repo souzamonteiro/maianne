4d1b6c5ff5c4450ba6f0a5f4e8ec8d2ef7075f05
Roberto Luiz Souza Monteiro <roberto@souzamonteiro.com>
2022-05-15 11:31:00 -0300

    Some work on fix training an ANN

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #./bin/maianne.maia --create --topology mlp --ni 1 --no 1 --nl 1 --nhu 3 -o test/mlp.net
-./bin/maianne.maia --its test/its-01.csv --ots test/ots-01.csv --lrate 0.005 --af tanh --oaf linear --of none --ofc "[1, 0]" --loops --prepare --nw --epochs 2000 --correct 0.001 --interval 100 test/mlp.net
+./bin/maianne.maia --ann --its test/its-01.csv --ots test/ots-01.csv --lrate 0.005 --af tanh --oaf linear --of none --ofc "[1, 0]" --loops --prepare --nw --epochs 2000 --correct 0.001 --interval 100 test/mlp.net
 ./bin/maianne.maia -i test/in-01.csv --ni 1 --no 1 --af tanh --oaf linear --of none --ofc "[1, 0]" test/mlp-trained.net
 
 #./bin/maianne.maia --normalize -i test/BRASILIA_A001_2021-06-01_2021-12-31-Temperature.csv
