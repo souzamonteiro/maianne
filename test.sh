8f218a366c8a2f3f06472f7b40cf5f0780e25867
souzamonteiro <roberto@souzamonteiro.com>
2022-05-16 13:24:03 +0000

    Fixed bug in training ANNs

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 #./bin/maianne.maia --create --topology mlp --ni 1 --no 1 --nl 1 --nhu 3 -o test/mlp.net
-./bin/maianne.maia --ann --its test/its-01.csv --ots test/ots-01.csv --lrate 0.005 --af tanh --oaf linear --of none --ofc "[1, 0]" --loops --prepare --nw --epochs 2000 --correct 0.001 --interval 100 test/mlp.net
-./bin/maianne.maia -i test/in-01.csv --ni 1 --no 1 --af tanh --oaf linear --of none --ofc "[1, 0]" test/mlp-trained.net
+#./bin/maianne.maia --ann mlp.net --its test/its-01.csv --ots test/ots-01.csv --lrate 0.005 --af tanh --oaf linear --of none --ofc "[1, 0]" --loops --prepare --nw --epochs 2000 --correct 0.001 --interval 100 test/mlp.net
+#./bin/maianne.maia -i test/in-01.csv --ni 1 --no 1 --af tanh --oaf linear --of none --ofc "[1, 0]" --loops test/mlp-trained.net
 
 #./bin/maianne.maia --normalize -i test/BRASILIA_A001_2021-06-01_2021-12-31-Temperature.csv
 #./bin/maianne.maia --denormalize -i test/BRASILIA_A001_2021-06-01_2021-12-31-Temperature-normalized.csv --minimum 8.1 --maximum 34.8
